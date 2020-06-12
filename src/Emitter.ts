7e5c04a58244e861b751d67d39c4f9de2ec82efa
Joshua Amaju <princejosh0007@gmail.com>
2020-06-12 11:21:48 +0100

    add types support for events

--- src/Emitter.ts
+++ src/Emitter.ts
@@ -1,38 +1,39 @@
-type Listener = (...args: any[]) => void;
+export type Listener = (...args: any[]) => void;
 
-type Listeners = Map<string, Listener[]>;
+type Listeners = Map<string, Map<number, Listener>>;
 
-type Dispatcher = {
+export type Dispatcher = {
   type: string;
   [key: string]: any;
 };
 
-export default class Emitter {
+export default class Emitter<E extends Dispatcher = any> {
+  uuid = 0;
   listeners: Listeners = new Map();
 
-  on(event: string, fn: Listener) {
-    const existing = this.listeners.get(event) ?? [];
-    this.listeners.set(event, existing.concat(fn));
-  }
+  subscribe(event: E["type"], fn: Listener) {
+    const id = ++this.uuid;
+    const existing = this.listeners.get(event) ?? new Map();
+    this.listeners.set(event, existing.set(id, fn));
 
-  off(event: string, fn: Listener) {
-    const existing = this.listeners.get(event);
-
-    if (existing) {
-      const fns = existing.filter((listener) => listener === fn);
-      this.listeners.set(event, fns);
-    }
+    return () => {
+      this.listeners.get(event).delete(id);
+      this.uuid -= 1;
+    };
   }
 
   clear(event: string) {
-    this.listeners.set(event, []);
+    const listeners = this.listeners.get(event);
+    this.uuid -= listeners.size;
+    listeners.clear();
   }
 
   clearAll() {
+    this.uuid = 0;
     this.listeners.clear();
   }
 
-  send<T extends Dispatcher>(e: T | T["type"]) {
+  send(e: E | E["type"]) {
     const { type, ...args } = typeof e === "string" ? { type: e } : e;
     const listeners = this.listeners.get(type);
 
