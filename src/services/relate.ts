ce0ca37b0a228a5d7da57705756bfba3f3faf481
Joshua Amaju <princejosh0007@gmail.com>
2020-06-20 21:25:33 +0100

    move relation creation to an external helper function

--- src/services/relate.ts
+++ src/services/relate.ts
@@ -1,60 +1,50 @@
 import { Query } from "../query/types";
-import returnFormatter from "../utils/returnFormatter";
-import { getProps, relationStoreName } from "../utils/utils";
+import returnFormatter from "./utils/returnFormatter";
 import { Properties, ReturnOperator } from "./types";
+import { relationStoreName } from "../utils/utils";
+import { getProps, relateHelper } from "./utils/utils";
 
-export default function relate(
+export default async function relate(
   db: IDBDatabase,
   query: Query<string, Properties>,
   operators?: ReturnOperator
 ): Promise<Record<string, Properties>> {
   const returner = operators?.return;
   const { end, start, relationship } = query;
-  const { type, props } = relationship;
+  const { type } = relationship;
 
   const endPrimaryKey = end.props._id;
   const startPrimaryKey = start.props._id;
 
-  const relation = {
-    type,
-    to: end.label,
-    from: start.label,
-    end: endPrimaryKey,
-    start: startPrimaryKey,
-    ...props,
-  };
+  const tx = db.transaction(relationStoreName, "readwrite");
+
+  const res = ((await relateHelper(
+    tx,
+    query,
+    startPrimaryKey,
+    endPrimaryKey
+  )) as unknown) as IDBValidKey;
+
+  if (!returner) return;
 
   return new Promise((resolve, reject) => {
-    const tx = db.transaction(relationStoreName, "readwrite");
     const store = tx.objectStore(relationStoreName);
-    const req = store.put(relation);
+    const newReq = store.get(res);
 
-    req.onerror = () => reject(req.error);
+    newReq.onerror = () => reject(newReq.error);
 
-    req.onsuccess = () => {
-      const res = req.result;
+    newReq.onsuccess = () => {
+      const props = getProps(newReq.result);
 
-      if (!returner) return resolve();
+      const obj = {
+        [relationship.as]: {
+          type,
+          _id: res,
+          ...props,
+        },
+      } as Record<string, Properties>;
 
-      const newReq = store.get(res);
-      newReq.onerror = () => reject(newReq.error);
-
-      newReq.onsuccess = () => {
-        const props = getProps(newReq.result);
-
-        const obj = {
-          [relationship.as]: {
-            type,
-            _id: res,
-            ...props,
-          },
-        } as Record<string, Properties>;
-
-        resolve(returnFormatter(obj, returner));
-      };
+      resolve(returnFormatter(obj, returner));
     };
-
-    tx.onerror = () => reject(tx.error);
-    tx.onabort = () => reject(tx.error);
   });
 }
