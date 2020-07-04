4a3e69d15bfe187ba35c325b945ac30dd12106af
Joshua Amaju <princejosh0007@gmail.com>
2020-07-04 01:41:51 +0100

    install schema by default

--- src/openDB.ts
+++ src/openDB.ts
@@ -1,22 +1,25 @@
 import WeBase from "./WeBase";
 
-export default async function openDB(name: string, version: number): Promise<WeBase> {
-    const db = new WeBase(name, version);
-  
-    //   db.onUpgrade(async ({ schema }) => {
-    //     await schema.install();
-    //   });
-  
-    //   db.onBlocked(() => {
-    //     alert("Another version of this website is preventing an update.");
-    //   });
-  
-    //   db.onVersionChange(() => {
-    //     const message = "There is a new version, press ok to reload.";
-    //     if (confirm(message)) location.reload();
-    //   });
+export default async function openDB(
+  name: string,
+  version: number
+): Promise<WeBase> {
+  const db = new WeBase(name, version);
 
-    await db.connect();
-  
-    return db;
-  }
+  db.onUpgrade(async ({ schema }) => {
+    await schema.install();
+  });
+
+  //   db.onBlocked(() => {
+  //     alert("Another version of this website is preventing an update.");
+  //   });
+
+  //   db.onVersionChange(() => {
+  //     const message = "There is a new version, press ok to reload.";
+  //     if (confirm(message)) location.reload();
+  //   });
+
+  await db.connect();
+
+  return db;
+}
